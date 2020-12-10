b44559a10a4a27316c04143b55f443ec4c8669c7
elainehowyl <elaine_0110@hotmail.com>
2020-12-10 19:42:12 +0000

    buttons not resetting to white

---
+++ topic-6/src/components/Box.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-bind:style="{backgroundColor:bgColor}" class="boxes">
+        {{content}}
+        <button v-bind:click="reset">Reset</button>
+  </div>
+</template>
+<script>
+export default {
+    data:function(){
+        return{
+
+        }
+    },
+    props:['bgColor','content'],
+    methods:{
+        'reset':function(){
+           this.$emit('reset_backgroundcolor')
+        }
+    }
+}
+</script>
+<style scoped>
+  .boxes{
+      height:200px;
+      width:200px;
+      border:solid;
+  }
+</style>
---
+++ topic-6/src/components/ControlPanel.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+      <div class="row">
+          <Box v-bind:bgColor="box1Color" content="1" v-on:reset_backgroundcolor='resetBox1'></Box>
+          <Box v-bind:bgColor="box2Color" content="2" v-on:reset_backgroundcolor='resetBox2'></Box>
+          <Box v-bind:bgColor="box3Color" content="3" v-on:reset_backgroundcolor='resetBox3'></Box>
+      </div>
+      <h1>Select box color</h1>
+      <div>
+       <label>Box 1 </label>
+       <select v-model="box1Color">
+          <option value="red">Red</option>
+          <option value="green">Green</option>
+          <option value="blue">Blue</option>
+       </select>
+      </div>
+      <div>
+       <label>Box 2 </label>
+       <select v-model="box2Color">
+          <option value="red">Red</option>
+          <option value="green">Green</option>
+          <option value="blue">Blue</option>
+       </select>
+      </div>
+      <div>
+       <label>Box 3 </label>
+       <select v-model="box3Color">
+          <option value="red">Red</option>
+          <option value="green">Green</option>
+          <option value="blue">Blue</option>
+       </select>
+      </div>
+  </div>
+</template>
+<script>
+import Box from "./Box"
+export default {
+    components:{
+        Box
+    },
+    data:function(){
+        return{
+           'box1Color':'',
+           'box2Color':'',
+           'box3Color':'',
+        }
+    },
+    methods:{
+        'resetBox1':function(){
+            this.box1Color='white'
+        },
+        'resetBox2':function(){
+            this.box2Color='white'
+        },
+        'resetBox3':function(){
+            this.box3Color='white'
+        }
+    }
+    
+}
+</script>
+<style scoped>
+   .row{
+       display:flex;
+       justify-content:center;
+   }
+</style>
