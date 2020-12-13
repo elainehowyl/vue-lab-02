037a74e464d4345aab4ed65f480ec1fdab2d8687
elainehowyl <elaine_0110@hotmail.com>
2020-12-13 11:51:32 +0000

    finished hands-on 8.1

---
+++ topic-8/src/components/Product.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h4>{{product.name}}</h4>
+        <ul>
+            <li>Price: {{product.price}}</li>
+            <li>Type: {{product.type}}</li>
+            <li>Characteristics: {{product.characteristics}}</li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    props:['product']
+}
+</script>
+<style scoped>
+
+</style>
--- topic-8/src/components/ProductListAdvanced.vue
+++ topic-8/src/components/ProductListAdvanced.vue
@@ -1,11 +1,51 @@
+// Create a catalog of products, each product having the following properties:
+// Name of product
+// Price of the product
+// Type of product (whether it is digital or physical)
+// Whether the product has one or more of the following characteristics:
+// Fragile
+// Discount
+// Limited edition
+
+// Using the techniques in this section, create a ProductListAdvanced and Product component. 
+// The former component should render a list of Product components.
+
 <template>
-    <div></div>
+    <div>
+        <Product v-for="p in products" v-bind:key="p.id" v-bind:product="p"></Product>
+    </div>
 </template>
 <script>
+import Product from "./Product"
 export default {
+    components:{
+        Product
+    },
     data:function(){
         return {
-            gg
+            products:[
+                {
+                    id:'001',
+                    name:'poster',
+                    price:15,
+                    type:'physical',
+                    characteristics:['limited edition', 'discount']
+                },
+                {
+                    id:'002',
+                    name:'album',
+                    price:40,
+                    type:'digital',
+                    characteristics:['limited edition']
+                },
+                {
+                    id:'003',
+                    name:'lightstick',
+                    price:20,
+                    type:'physical',
+                    characteristics:['fragile']
+                }
+            ]
         }
     }
 }
